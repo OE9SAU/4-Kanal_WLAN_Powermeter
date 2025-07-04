39a9b3b98d4bd3a65f4c1104c696e9116629d131
Martin <114362733+OE9SAU@users.noreply.github.com>
2025-07-04 07:56:19 +0200

    Lib + uC update

--- µC-Code/wemos_d1_oled_webserver_AP/wemos_d1_oled_webserver_AP.ino
+++ µC-Code/wemos_d1_oled_webserver_AP/wemos_d1_oled_webserver_AP.ino
@@ -1,15 +1,15 @@
 #include <Wire.h>
-#include <SH1106Wire.h>   	  // Von ESP8266-OLED Bibliothek https://github.com/ThingPulse/esp8266-oled-ssd1306
-#include <INA226.h>	          // https://github.com/RobTillaart/INA226
+#include <SH1106Wire.h>   	  	// https://github.com/ThingPulse/esp8266-oled-ssd1306
+#include <INA226.h>	          	// https://github.com/RobTillaart/INA226
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <WiFiManager.h>  		// https://github.com/tzapu/WiFiManager
 
-const char* Release = "V1.0 by OE9SAU";
+const char* Release = "V2.0 by OE9SAU";
 
-// Display: SH1106 128x64, I2C
+// OLED Display
 SH1106Wire display(0x3C, D2, D1);  // SDA = D2, SCL = D1
 
 // INA226 Sensoren
@@ -18,16 +18,10 @@ INA226 INA1(0x41);
 INA226 INA2(0x44);
 INA226 INA3(0x45);
 
-const char* ssid = "***;
-const char* password = "*****";
-
-IPAddress ip(192, 168, 1, 220);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
-IPAddress dns(8, 8, 8, 8);
-
+// Webserver
 ESP8266WebServer server(5050);
 
+// Sensorwerte
 float c00, v0, c11, v1, c22, v2, c33, v3;
 
 bool wlanOk = false;
@@ -59,6 +53,30 @@ void handleRoot() {
   server.send(200, "text/html", html);
 }
 
+// Signalstärke anzeigen (WLAN Balken rechts oben)
+void drawWifiSignal(int strength) {
+  int x = 110;
+  int y = 0;
+  int barWidth = 3;
+  int gap = 2;
+
+  int level = 0;
+  if (strength > -50) level = 4;
+  else if (strength > -60) level = 3;
+  else if (strength > -70) level = 2;
+  else if (strength > -80) level = 1;
+  else level = 0;
+
+  for (int i = 0; i < 4; i++) {
+    int barHeight = (i + 1) * 2;
+    if (i < level) {
+      display.fillRect(x + i * (barWidth + gap), y + (8 - barHeight), barWidth, barHeight);
+    } else {
+      display.drawRect(x + i * (barWidth + gap), y + (8 - barHeight), barWidth, barHeight);
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   Wire.begin(D2, D1);
@@ -70,10 +88,9 @@ void setup() {
   display.drawString(0, 0, "OE9XVI - PsMon");
   display.drawString(0, 12, Release);
   display.display();
-
   delay(1500);
 
-  // INA226 initialisieren
+  // INA226 Initialisierung
   if (!INA0.begin()) Serial.println("INA0 Fehler");
   if (!INA1.begin()) Serial.println("INA1 Fehler");
   if (!INA2.begin()) Serial.println("INA2 Fehler");
@@ -84,48 +101,42 @@ void setup() {
   INA2.setMaxCurrentShunt(20, 0.002);
   INA3.setMaxCurrentShunt(20, 0.002);
 
-  // WLAN-Konfiguration
-  WiFi.mode(WIFI_STA);
-  //WiFi.config(ip, gateway, subnet, dns);
-  WiFi.begin(ssid, password);
-
-  Serial.print("Verbinde mit WLAN");
-  display.drawString(0, 36, "Verbinde mit WLAN");
+  // WLAN mit WiFiManager
+  WiFiManager wifiManager;
+  display.drawString(0, 36, "Starte WiFi-Setup...");
   display.display();
-  unsigned long startAttemptTime = millis();
-  const unsigned long wifiTimeout = 10000; // 10 Sekunden Timeout
 
-  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < wifiTimeout) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  if (WiFi.status() == WL_CONNECTED) {
-    wlanOk = true;
-    Serial.println("\nWLAN verbunden, IP: " + WiFi.localIP().toString());
-    display.drawString(0, 48, "\nWLAN verbunden, IP: " + WiFi.localIP().toString());
-    display.display();
-    // OTA Setup
-    ArduinoOTA.onStart([]() { Serial.println("OTA Start"); });
-    ArduinoOTA.onEnd([]() { Serial.println("\nOTA Ende"); });
-    ArduinoOTA.onError([](ota_error_t error) {
-      Serial.printf("OTA Fehler [%u]: ", error);
-    });
-    ArduinoOTA.begin();
-
-    // Webserver Setup
-    server.on("/", handleRoot);
-    server.begin();
-    Serial.println("HTTP Server gestartet");
-  } else {
-    wlanOk = false;
-    Serial.println("\nWLAN-Verbindung fehlgeschlagen. Starte nur Display-Modus.");
-    display.clear();
-    display.drawString(0, 0, "WLAN-Fehler");
-    display.drawString(0, 12, "Nur OLED aktiv");
+  if (!wifiManager.autoConnect("OE9XVI-PsMon-AP")) {
+    Serial.println("WLAN-Verbindung fehlgeschlagen. Neustart...");
+    display.drawString(0, 48, "WLAN fehlgeschlagen");
     display.display();
     delay(3000);
+    ESP.restart();
+    delay(5000);
   }
+
+  // WLAN verbunden
+  wlanOk = true;
+  Serial.println("WLAN verbunden, IP: " + WiFi.localIP().toString());
+  display.clear();
+  display.drawString(0, 0, "WLAN verbunden");
+  display.drawString(0, 12, WiFi.SSID());
+  display.drawString(0, 24, WiFi.localIP().toString());
+  display.display();
+  delay(3000);  // 3 Sekunden anzeigen
+
+  // OTA Setup
+  ArduinoOTA.onStart([]() { Serial.println("OTA Start"); });
+  ArduinoOTA.onEnd([]() { Serial.println("\nOTA Ende"); });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("OTA Fehler [%u]\n", error);
+  });
+  ArduinoOTA.begin();
+
+  // Webserver
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP Server gestartet");
 }
 
 void loop() {
@@ -144,8 +155,9 @@ void loop() {
   c33 = INA3.getCurrent_mA() / 1000.0;
   v3 = INA3.getBusVoltage();
 
-  // OLED-Display aktualisieren
+  // OLED aktualisieren
   display.clear();
+  drawWifiSignal(WiFi.RSSI());  // Signalstärke-Symbol oben rechts
   display.drawString(0, 0, "OE9XVI - PsMon");
   display.drawString(0, 12, "0: " + String(c00, 2) + "A " + String(v0, 2) + "V");
   display.drawString(0, 24, "1: " + String(c11, 2) + "A " + String(v1, 2) + "V");
